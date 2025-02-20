13f10dcc7b00a33238b576eea0ff1f2612d9976e
Rodrigo Vieira <rodrigo.vieira.t0126458@edu.atec.pt>
2025-02-20 09:55:53 +0000

    Elaboração de exercício 1

--- exercicio1/exercicio1.c
+++ exercicio1/exercicio1.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World!\n");
+    int num = 0;
+    for(int i = 0; i < 10; i++) {
+        num+=2;
+        printf("Num = %d, i = %d", num, i);
+        printf("Aqui FOR");
+    }
+
+    return 0;
 }

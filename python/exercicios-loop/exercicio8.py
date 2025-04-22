07fa9a31186143262bf9061d829f5771f1967b1c
Rodrigo Vieira <rodrigo.vieira.t0126458@edu.atec.pt>
2025-04-22 13:35:52 +0100

    Python exercício de loops 8

---
+++ python/exercicios-loop/exercicio8.py
@@ -0,0 +1,16 @@
+i = 10
+while i <= 1000:
+    print(i, end = "")
+    if i != 1000:
+        print(end=",")
+    else:
+        print()
+    i+=10
+i = 15
+while i <= 995:
+    print(i, end = "")
+    if i != 995:
+        print(end=",")
+    else:
+        print()
+    i+=10

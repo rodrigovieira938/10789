fe43f7d9f5a6364e22005caa876b6cce814e6e9d
Rodrigo Vieira <rodrigo.vieira.t0126458@edu.atec.pt>
2025-04-22 12:27:19 +0100

    Python exercício de loops 1

---
+++ python/exercicios-loop/exercicio1.py
@@ -0,0 +1,9 @@
+pares = []
+impares = []
+for i in range(1, 31):
+    if i % 2 == 0:
+        pares.append(i)
+    else:
+        impares.append(i)
+print(f"Primeiros 30 números pares: {pares}")
+print(f"Primeiros 30 números impares: {impares}")
